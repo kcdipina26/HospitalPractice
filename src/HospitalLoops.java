cd62325454e9a790621cb6182e2e0bab85e8e502
kcdipina26 <dipinakc89@gmail.com>
2026-04-29 23:57:16 -0400

    Starting out loops

--- src/HospitalLoops.java
+++ src/HospitalLoops.java
@@ -1,8 +1,26 @@
 public class HospitalLoops {
 
+//This file is to learn and practice loops in the hospital billing system application in java
+/*
+Question 1: for loops, Print a bill reminder for 50 patients using a for loop. Output should be:
+Print a bill reminder for 500 patient using a for loop. Output should be:
+-Reminder: Bill due for patient 1
+-Reminder: Bill due for patient 2
+-Reminder: Bill due for patient 3
+-Reminder: Bill due for patient 4
+-Reminder: Bill due for patient 5
+ */
+
     public static void main(String[] args) {
-
-        System.out.println("Dipina is going to do if/else/if statements");
-
+        for (int i = 1; i <= 50; i = i + 1) {
+            System.out.println("Reminder: Bill due for patient "+ i);
+        }
     }
 }
+
+
+
+
+
+
+
