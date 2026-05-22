672936cd5d64c6f44997418367d353318bb41637
kcdipina26 <dipinakc89@gmail.com>
2026-05-22 16:07:29 -0400

    Constructor practice and built out for hospital billing system

--- src/Patient.java
+++ src/Patient.java
@@ -13,7 +13,24 @@ public class Patient{                         //create patient class
  boolean hasInsurance;
  double totalBill;
 
- public static void main(String[] args){             //main method
+ public Patient(){                 //This is first constructor for 3 hand-written patient objects
+
+
+ }
+
+ //This is the constructor for new objects to be built and avoid hand typing for individual patient
+public Patient(String name, int age, String bloodType, boolean hasInsurance, double totalBill) {
+    this.name = name;
+    this.age = age;
+    this.bloodType = bloodType;
+    this.hasInsurance = hasInsurance;
+    this.totalBill = totalBill;
+
+}
+
+
+//Main method
+ public static void main(String[] args){
      Patient patient1 = new Patient();              //created an object called patient1
      patient1.name = "Deepika";                     //filled out all the necessary fields for patient1 object
      patient1.age = 30;
@@ -37,10 +54,21 @@ public class Patient{                         //create patient class
      patient3.bloodType = "B+";
      patient3.hasInsurance = true;
      patient3.totalBill = 220.00;
-     System.out.println("Patient 3" + " Name " + patient3.name + " Age " + patient3.age + " Patient Blood type " + patient3.bloodType + " Has Insurance: " + patient3.hasInsurance + " Total Bill : $" + patient3.totalBill);
+     System.out.println("Patient 3 " + " Name " + patient3.name + " Age " + patient3.age + " Patient Blood type " + patient3.bloodType + " Has Insurance: " + patient3.hasInsurance + " Total Bill : $" + patient3.totalBill);
+
+//Creating object constructor in use helps create object faster in a real application scenario
+     Patient patient4 = new Patient("Diva", 19, "A+", true, 120.67); //patient 4
+     Patient patient5 = new Patient("Ramesh", 38, "B+", false, 390.77); //patient 5
+     Patient patient6 = new Patient("Dil", 48, "A+", true, 778.90);  //patient 6
+     Patient patient7 = new Patient("Debaki", 67, "A+", false, 450.66); //patient 7
+     System.out.println("Patient 4 " + " Name " + patient4.name + " Age " + patient4.age + " Patient Blood type " + patient4.bloodType + " Has Insurance: " + patient4.hasInsurance + " Total Bill : $" + patient4.totalBill);
+     System.out.println("Patient 5 " + " Name " + patient5.name + " Age " + patient5.age + " Patient Blood type " + patient5.bloodType + " Has Insurance: " + patient5.hasInsurance + " Total Bill : $" + patient5.totalBill);
+     System.out.println("Patient 6 " + " Name " + patient6.name + " Age " + patient6.age + " Patient Blood type " + patient6.bloodType + " Has Insurance: " + patient6.hasInsurance + " Total Bill : $" + patient6.totalBill);
+     System.out.println("Patient 7 " + " Name " + patient7.name + " Age " + patient7.age + " Patient Blood type " + patient7.bloodType + " Has Insurance: " + patient7.hasInsurance + " Total Bill : $" + patient7.totalBill);
 
 
  }
+
 
 
 
