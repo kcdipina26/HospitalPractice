e12dd4642beaea4202776d1fda5650821d0509de
kcdipina26 <dipinakc89@gmail.com>
2026-06-06 10:42:43 -0400

    Hospital Billing System inheritance practice intro - Java v0

--- src/EmergencyPatient.java
+++ src/EmergencyPatient.java
@@ -1,6 +1,40 @@
 //In this file we are going to be practicing OOP principle for inheritance, where we have superclass (parent) and subclass (child)
 
 
-public class EmergencyPatient extends Patient {
-    private String emergencyLevel;                       //"Critical", "High", "Normal"
-}
+public class EmergencyPatient extends Patient {            //Emergency Patient Inherits everything from Patient automatically.
+    //Therefore name, age, Bloodtype, hasInsurance, totalBill all exist already no need to write them as OOP principle is reusability
+     String emergencyLevel;
+     double ptBodyTemp;
+     int ptBloodPressure;
+     double ptLabResults;
+
+
+    //Use super to call the parent class patient constructor
+    public EmergencyPatient(String emergencyLevel, double ptBodyTemp, int ptBloodPressure, double ptLabResults) {
+        //Calling the super from parent class, getting error because no arg constructor but i have that in parent class? how to fix it call different parent class constructor need to understand
+        super();
+        //Defining the constructor the Emergency patient
+        this.emergencyLevel = emergencyLevel;
+        this.ptBodyTemp = ptBodyTemp;
+        this.ptBloodPressure = ptBloodPressure;
+        this.ptLabResults = ptLabResults;
+
+    }
+
+
+    //There has to be override methods specific to the patient but right now i do not have them not sure what to override?
+//    @Override
+//
+//    public void ptCritical() {
+//        super.getHasInsurance();
+//        System.out.println("Regardless of insurance admit patient immediately");
+
+
+
+    }
+
+
+
+
+
+
--- src/Patient.java
+++ src/Patient.java
@@ -4,95 +4,96 @@ This java file consist of learning the 1st principles of OOPS. Create a new file
 Then in main create 2 patient objects with different values and print all their details.
  */
 
+public  class Patient {                         //create patient class
+
+    public String name;                        //encapsulated the fields & added fields or attributes to the blueprint of patient class
+    public int age;
+    public String bloodType;
+    public boolean hasInsurance;
+    public double totalBill;
+
+    public Patient() {                 //This is first constructor for 3 hand-written patient objects
 
 
-public class Patient{                         //create patient class
- private String name;                        //encapsulated the fields & added fields or attributes to the blueprint of patient class
- private int age;
- private String bloodType;
- private boolean hasInsurance;
- private double totalBill;
-
- public Patient(){                 //This is first constructor for 3 hand-written patient objects
-
-
- }
-
- //This is the constructor for new objects to be built and avoid hand typing for individual patient
-public Patient(String name, int age, String bloodType, boolean hasInsurance, double totalBill) {
-    this.name = name;
-    this.age = age;
-    this.bloodType = bloodType;
-    this.hasInsurance = hasInsurance;
-    this.totalBill = totalBill;
-
-}
-
-//Writing getters for each fields same with UpperCamelCasing:  name, age, bloodType, hasInsurance, totalbill
-    public String getName(){
-     return name;
     }
 
-    public int getAge(){
-     return age;
+    //This is the constructor for new objects to be built and avoid hand typing for individual patient
+    public Patient(String name, int age, String bloodType, boolean hasInsurance, double totalBill) {
+        this.name = name;
+        this.age = age;
+        this.bloodType = bloodType;
+        this.hasInsurance = hasInsurance;
+        this.totalBill = totalBill;
+
     }
 
-    public String getBloodType(){
-     return bloodType;
+    //Writing getters for each fields same with UpperCamelCasing:  name, age, bloodType, hasInsurance, totalbill
+    public String getName() {
+        return name;
     }
 
-    public boolean getHasInsurance(){
-     return hasInsurance;
+    public int getAge() {
+        return age;
     }
 
-    public double getTotalBill(){
-     return totalBill;
+    public String getBloodType() {
+        return bloodType;
     }
 
-  //Setters for the patient so no one can change it into wrong data type & values.
+    public boolean getHasInsurance() {
+        return hasInsurance;
+    }
 
-  public void setName(String name){
-     //Setting a logical branching so its strictly name and not characters or numbers
-      this.name = name;
+    public double getTotalBill() {
+        return totalBill;
+    }
 
-  }
+    //Setters for the patient so no one can change it into wrong data type & values.
 
-  public void setAge(int age){
-     //Setting a logical branching for age to avoid negative or unrealistic values
-      if(age > 0 && age < 130){
-          this.age = age;
-      }
-  }
+    public void setName(String name) {
+        //Setting a logical branching so its strictly name and not characters or numbers
+        this.name = name;
 
-  public void setBloodType(String bloodType) {
-      //set bloodtype as a collection of array it can only be those variables.
-      //check if blood type exist in the valid arrays
-      String[] bloodGroup = new String[]{"A+", "A-", "AB+", "AB-", "O-", "O+"}; //bloodGroup is a collection of string of arrays, as blood group can only be these 5
-      for (int i = 0; i < bloodGroup.length; i++) {                             //for loop to only give possible 5 blood groups
-          if (bloodGroup[i].equals(bloodType)) {                               //equal the string [] with string bloodType parameter
-              this.bloodType = bloodType;                                      //therefore if bloodGroup = bloodtype then temporary blootype on the right = bloodtype permanent on the left
-              return;
-          }
-      }
-  }
-  public void setHasInsurance(boolean hasInsurance){
-     this.hasInsurance = hasInsurance;
-  }
+    }
 
-  public void setTotalBill(double totalBill){
-     if(totalBill > 0.00){
-         this.totalBill = totalBill;
-     }
-  }
-//Main method
- public static void main(String[] args){
-     Patient patient1 = new Patient();              //created an object called patient1
-     patient1.setName("Deepika");                    //filled out all the necessary fields for patient1 object
-     patient1.setAge(30);
-     patient1.setBloodType("A+");
-     patient1.setHasInsurance(true);
-     patient1.setTotalBill(230.98);
-     System.out.println( "Patient 1 "+" Name " + patient1.getName() + " Age " + patient1.getAge() + " Patient Blood type: " + patient1.getBloodType() + " Has Insurance: " + patient1.getHasInsurance() + " Total Bill : $" + patient1.getTotalBill());
+    public void setAge(int age) {
+        //Setting a logical branching for age to avoid negative or unrealistic values
+        if (age > 0 && age < 130) {
+            this.age = age;
+        }
+    }
+
+    public void setBloodType(String bloodType) {
+        //set bloodtype as a collection of array it can only be those variables.
+        //check if blood type exist in the valid arrays
+        String[] bloodGroup = new String[]{"A+", "A-", "AB+", "AB-", "O-", "O+"}; //bloodGroup is a collection of string of arrays, as blood group can only be these 5
+        for (int i = 0; i < bloodGroup.length; i++) {                             //for loop to only give possible 5 blood groups
+            if (bloodGroup[i].equals(bloodType)) {                               //equal the string [] with string bloodType parameter
+                this.bloodType = bloodType;                                      //therefore if bloodGroup = bloodtype then temporary blootype on the right = bloodtype permanent on the left
+                return;
+            }
+        }
+    }
+
+    public void setHasInsurance(boolean hasInsurance) {
+        this.hasInsurance = hasInsurance;
+    }
+
+    public void setTotalBill(double totalBill) {
+        if (totalBill > 0.00) {
+            this.totalBill = totalBill;
+        }
+    }
+
+    //Main method
+    public static void main(String[] args) {
+        Patient patient1 = new Patient();              //created an object called patient1
+        patient1.setName("Deepika");                    //filled out all the necessary fields for patient1 object
+        patient1.setAge(30);
+        patient1.setBloodType("A+");
+        patient1.setHasInsurance(true);
+        patient1.setTotalBill(230.98);
+        System.out.println("Patient 1 " + " Name " + patient1.getName() + " Age " + patient1.getAge() + " Patient Blood type: " + patient1.getBloodType() + " Has Insurance: " + patient1.getHasInsurance() + " Total Bill : $" + patient1.getTotalBill());
 
     /*
     Patient patient2 = new Patient();                  created second object called patient 2 without using getters and setters , constructor to see the difference
@@ -114,58 +115,36 @@ public Patient(String name, int age, String bloodType, boolean hasInsurance, dou
 */
 
 //Redoing patient 2 and patient 3 with utilization of getters and setters.
-     Patient patient2 = new Patient();
-     patient2.setName("Dipina");
-     patient2.setAge(30);
-     patient2.setBloodType("C+");                          //testing blood group so see if it returns null. 
-     patient2.setHasInsurance(false);
-     patient2.setTotalBill(235.86);
-     System.out.println("Patient 2" + " Name " + patient2.getName() + " Age "+ patient2.getAge()+ " Patient Blood Type " + patient2.getBloodType()+ " Has Insurance: " + patient2.getHasInsurance() + " Total Bill: $" + patient2.getTotalBill());
-
-     Patient patient3 = new Patient();
-     patient3.setName("Dil");
-     patient3.setAge(56);
-     patient3.setBloodType("A+");
-     patient3.setHasInsurance(false);
-     patient3.setTotalBill(456.66);
-     System.out.println("Patient 3" + " Name " + patient3.getName() + " Age " + patient3.getAge() + " Patient Blood Type " + patient3.getBloodType() + " Has Insurance: " + patient3.getHasInsurance() + " Total Bill: $ " + patient3.getTotalBill());
-//Creating object constructor in use helps create object faster in a real application scenario
-//Setter and Getter
-     Patient patient4 = new Patient("Diva", 19, "A+", true, 120.67); //patient 4
-     Patient patient5 = new Patient("Ramesh", 38, "B+", false, 390.77); //patient 5
-     Patient patient6 = new Patient("Dil", 48, "A+", true, 778.90);  //patient 6
-     Patient patient7 = new Patient("Debaki", 67, "A+", false, 450.66); //patient 7
-     System.out.println("Patient 4 " + " Name " + patient4.getName() + " Age " + patient4.getAge() + " Patient Blood type " + patient4.getBloodType() + " Has Insurance: " + patient4.getHasInsurance() + " Total Bill : $" + patient4.getTotalBill());
-     System.out.println("Patient 5 " + " Name " + patient5.getName() + " Age " + patient5.getAge() + " Patient Blood type " + patient5.getBloodType()+ " Has Insurance: " + patient5.getHasInsurance() + " Total Bill : $" + patient5.getTotalBill());
-     System.out.println("Patient 6 " + " Name " + patient6.getName() + " Age " + patient6.getAge() + " Patient Blood type " + patient6.getBloodType() + " Has Insurance: " + patient6.getHasInsurance() + " Total Bill : $" + patient6.getTotalBill());
-     System.out.println("Patient 7 " + " Name " + patient7.getName() + " Age " + patient7.getAge() + " Patient Blood type " + patient7.getBloodType() + " Has Insurance: " + patient7.getHasInsurance() + " Total Bill : $" + patient7.getTotalBill());
-
-
- }
+//     Patient patient2 = new Patient();
+//     patient2.setName("Dipina");
+//     patient2.setAge(30);
+//     patient2.setBloodType("C+");                          //testing blood group so see if it returns null.
+//     patient2.setHasInsurance(false);
+//     patient2.setTotalBill(235.86);
+//     System.out.println("Patient 2" + " Name " + patient2.getName() + " Age "+ patient2.getAge()+ " Patient Blood Type " + patient2.getBloodType()+ " Has Insurance: " + patient2.getHasInsurance() + " Total Bill: $" + patient2.getTotalBill());
+//
+//     Patient patient3 = new Patient();
+//     patient3.setName("Dil");
+//     patient3.setAge(56);
+//     patient3.setBloodType("A+");
+//     patient3.setHasInsurance(false);
+//     patient3.setTotalBill(456.66);
+//     System.out.println("Patient 3" + " Name " + patient3.getName() + " Age " + patient3.getAge() + " Patient Blood Type " + patient3.getBloodType() + " Has Insurance: " + patient3.getHasInsurance() + " Total Bill: $ " + patient3.getTotalBill());
+////Creating object constructor in use helps create object faster in a real application scenario
+////Setter and Getter
+        Patient patient4 = new Patient("Diva", 19, "A+", true, 120.67); //patient 4
+        Patient patient5 = new Patient("Ramesh", 38, "B+", false, 390.77); //patient 5
+        Patient patient6 = new Patient("Dil", 48, "A+", true, 778.90);  //patient 6
+        Patient patient7 = new Patient("Debaki", 67, "A+", false, 450.66); //patient 7
+        System.out.println("Patient 4 " + " Name " + patient4.getName() + " Age " + patient4.getAge() + " Patient Blood type " + patient4.getBloodType() + " Has Insurance: " + patient4.getHasInsurance() + " Total Bill : $" + patient4.getTotalBill());
+        System.out.println("Patient 5 " + " Name " + patient5.getName() + " Age " + patient5.getAge() + " Patient Blood type " + patient5.getBloodType() + " Has Insurance: " + patient5.getHasInsurance() + " Total Bill : $" + patient5.getTotalBill());
+        System.out.println("Patient 6 " + " Name " + patient6.getName() + " Age " + patient6.getAge() + " Patient Blood type " + patient6.getBloodType() + " Has Insurance: " + patient6.getHasInsurance() + " Total Bill : $" + patient6.getTotalBill());
+        System.out.println("Patient 7 " + " Name " + patient7.getName() + " Age " + patient7.getAge() + " Patient Blood type " + patient7.getBloodType() + " Has Insurance: " + patient7.getHasInsurance() + " Total Bill : $" + patient7.getTotalBill());
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 }
+
+
